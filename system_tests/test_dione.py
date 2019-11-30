511de5f9eb5fd4ffa83df39a287b8586326264dd
Chiao Hsieh <chsieh16@illinois.edu>
2019-11-30 11:10:26 -0600

    Update test script to use new class name

--- system_tests/test_dione.py
+++ system_tests/test_dione.py
@@ -1,11 +1,11 @@
 import ast
 
-from dione.frontend.ioa_ast_checker import IOAAstChecker
+from dione.frontend.dione_ast_checker import DioneAstChecker
 from dione.frontend.translator_dafny import TranslatorDafny
 
 with open("system_tests/ioa_examples/AsyncLCR.ioa.py") as file:
     tree = ast.parse(file.read())
-    IOAAstChecker().visit(tree)
+    DioneAstChecker().visit(tree)
 
     file.seek(0)
     translator = TranslatorDafny(file)
